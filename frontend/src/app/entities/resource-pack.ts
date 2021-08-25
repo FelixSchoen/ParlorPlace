1818431ce336d3e99b36d4fae6089fbdc7bf3abb
Felix Schön <mail@fschoen.com>
2021-08-25 15:45:13 +0200

    [#31] Change pack structure

--- frontend/src/app/entities/resource-pack.ts
+++ frontend/src/app/entities/resource-pack.ts
@@ -5,16 +5,61 @@ import {LanguageIdentifier} from "../enums/language-identifier";
 
 export abstract class ResourcePack {
 
+  // Paths to pack file, audio files
+  protected basePath: string = "../../assets/resourcepack/";
+  protected gamePath: string;
+  protected packPath: string;
+
+  // Which pack to choose
+  protected packName: string;
+  protected defaultPackName: string;
+
+  // JSON File
+  protected packFile: Promise<any>;
+
   protected constructor(protected loadJsonService: LoadJsonService) {
   }
 
-  protected basePath: string = "../../assets/resourcepack/";
-  protected packName: string;
-  protected gamePath: string;
-  protected packFile: Promise<any>;
-  protected defaultPackName: string;
+  // Code name
 
-  public abstract getCodeNameRepresentation(codeName: CodeName): Promise<string>;
+  public async getCodeNameVoiceLine(codeName: CodeName): Promise<VoiceLine> {
+    return this.pickRandom(await this.getVoiceLineArray("codename", codeName.valueOf().toLowerCase(), "voiceline"));
+  }
+
+  public async getCodeNameRepresentation(codeName: CodeName): Promise<string> {
+    return await this.packFile.then(value => value.codename[codeName.valueOf().toLowerCase()].name)
+  }
+
+  // Utility
+
+  public async getVoiceLineArray(...identifiers: string[]): Promise<VoiceLine[]> {
+    let voiceLineArray: VoiceLine[] = [];
+    let voiceLinePath: string = "";
+
+    let pointer: any = await this.packFile.then(value => value);
+
+    // Add path to voice line
+    identifiers.forEach((entry, index) => {
+      if (index != identifiers.length - 1)
+        voiceLinePath += (entry + "/");
+    })
+
+    // Derive actual entry from JSON structure
+    for (let identifier of identifiers) {
+      pointer = pointer[identifier];
+    }
+
+    // Add all given voice lines
+    for (let entry of pointer) {
+      voiceLineArray.push(VoiceLine.fromJson(this.packPath, voiceLinePath, this, entry))
+    }
+
+    return voiceLineArray;
+  }
+
+  public pickRandom(voiceLines: VoiceLine[]): VoiceLine {
+    return voiceLines[Math.floor(Math.random() * voiceLines.length)]
+  }
 
 }
 
@@ -34,38 +79,56 @@ export class WerewolfResourcePack extends ResourcePack {
     } else
       this.packName = packIdentifier.toLowerCase()
 
-    this.packFile = loadJsonService.loadJson(this.gamePath + this.packName + "/" + languageIdentifier.toLowerCase() + "/pack.json").toPromise();
+    this.packPath = this.gamePath + this.packName + "/" + languageIdentifier.toLowerCase() + "/"
+    this.packFile = loadJsonService.loadJson(this.packPath + "pack.json").toPromise();
   }
 
-  public async getCodeNameRepresentation(codeName: CodeName): Promise<string> {
-    return await this.packFile.then(value => value.codename[codeName.valueOf().toLowerCase()].name)
-  }
-
-  public async getCodeNameVoiceLine(codeName: CodeName): Promise<VoiceLine[]> {
-    let voiceLineArray: VoiceLine[] = []
-
-    for (let entry of await this.packFile.then(value => value.codename[codeName.valueOf().toLowerCase()].voiceline))
-      voiceLineArray.push(VoiceLine.fromJson(entry));
-
-    return voiceLineArray
+  public async getStartVoiceLine(): Promise<VoiceLine> {
+    return this.pickRandom(await this.getVoiceLineArray("voiceline", "general", "start"));
   }
 
 }
 
 export class VoiceLine {
 
+  // Paths
+  private packPath: string;
+  private voiceLinePath: string;
+
+  // Resource Pack to obtain voice lines of code names
+  private resourcePack: ResourcePack;
+
+  // Fields that store information about the voice line
   public files: string[] = [];
   public order: string[] = [];
+  private codeNames: VoiceLine[] = [];
+
+  // Static extension of the files, could perhaps be changed in the future
+  private extension: string = ".mp3"
 
   constructor() {
   }
 
-  public static fromJson(data: any): VoiceLine {
+  public static fromJson(packPath: string, voiceLinePath: string, resourcePack: ResourcePack, data: any): VoiceLine {
     let voiceLine = new VoiceLine();
 
+    voiceLine.packPath = packPath;
+    voiceLine.voiceLinePath = voiceLinePath;
+    voiceLine.resourcePack = resourcePack;
+
+    // Check audio files
     if (data.path != undefined) {
-      voiceLine.files = data.path
+      let files: string[] = [];
+
+      // Append actual path to entries
+      for (let entry of data.path) {
+        files.push(voiceLinePath + entry);
+      }
+
+      voiceLine.files = files;
     }
+
+    // Check order of files
     if (data.order != undefined) {
       voiceLine.order = data.order
     }
@@ -73,5 +136,64 @@ export class VoiceLine {
     return voiceLine;
   }
 
+  public toPathArray(): string[] {
+    let stringArray: string[] = [];
+
+    for (let entry of this.getOrderedPaths()) {
+      stringArray.push(this.packPath + entry + this.extension);
+    }
+
+    return stringArray;
+  }
+
+  public async primeWithCodeNames(codeNames: CodeName[]): Promise<void> {
+    let codeNameArray: VoiceLine[] = [];
+
+    for (let entry of codeNames) {
+      codeNameArray.push(await this.resourcePack.getCodeNameVoiceLine(entry).then(value => value))
+    }
+
+    this.codeNames = codeNameArray;
+  }
+
+  // Utility
+
+  private getOrderedPaths(): string[] {
+    if (this.order.length == 0)
+      return this.files;
+
+    let fileArray: string[] = [];
+
+    // Regular expressions in order to allow for users to specify the order of the voiceline
+    let regexFile = new RegExp("file_([0-9]+)");
+    let regexCodename = new RegExp("codename_([0-9]+)");
+
+    for (let directive of this.order) {
+
+      // Check if file matches
+      if (regexFile.test(directive)) {
+        let match = directive.match(regexFile)
+
+        if (match) {
+          let identifier = Number(match[1]) - 1
+          fileArray.push(this.files[identifier])
+        }
+
+      }
+
+      // Check if codename matches
+      if (regexCodename.test(directive)) {
+        let match = directive.match(regexCodename)
+
+        if (match) {
+          let identifier = Number(match[1]) - 1
+          fileArray.push(this.codeNames[identifier].getOrderedPaths()[0])
+        }
+
+      }
+    }
+
+    return fileArray;
+  }
 
 }
--- frontend/src/app/modules/experimental/experimental.component.ts
+++ frontend/src/app/modules/experimental/experimental.component.ts
@@ -2,7 +2,6 @@ import {Component} from "@angular/core";
 import {AudioService} from "../../services/audio.service";
 import {WerewolfResourcePack} from "../../entities/resource-pack";
 import {LoadJsonService} from "../../services/load-json.service";
-import {CodeName} from "../../enums/code-name";
 import {WerewolfResourcePackType} from "../../enums/games/werewolf-resource-pack-type";
 import {LanguageIdentifier} from "../../enums/language-identifier";
 
@@ -19,8 +18,9 @@ export class ExperimentalComponent {
   public action(): void {
     let pack = new WerewolfResourcePack(this.jsonService, WerewolfResourcePackType.OLD_MAN, LanguageIdentifier.DE);
 
-    pack.getCodeNameVoiceLine(CodeName.ALFA).then(value => console.log(value))
-    pack.getCodeNameRepresentation(CodeName.ALFA).then(value => console.log(value))
+    let voiceLine = pack.getStartVoiceLine();
+
+    voiceLine.then(value => this.voicelineService.playAudio(value.toPathArray()))
   }
 
 }
